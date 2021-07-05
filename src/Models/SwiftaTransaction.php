415f5daf20a5c1d71fbc316122937076523f2e95
Kamuran Engin <ke@inensus.com>
2021-07-05 19:29:31 +0300

    first commit

---
+++ src/Models/SwiftaTransaction.php
@@ -0,0 +1,31 @@
+<?php
+namespace Inensus\SwiftaPaymentProvider\Models;
+
+use App\Models\BaseModel;
+use App\Models\Transaction\Transaction;
+use App\Models\Transaction\TransactionConflicts;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class SwiftaTransaction extends BaseModel
+{
+    protected $table = 'swifta_transactions';
+    /**
+     * @return MorphOne
+     */
+    public function transaction(): MorphOne
+    {
+        return $this->morphOne(Transaction::class, 'original_transaction');
+    }
+
+    public function manufacturerTransaction(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function conflicts(): MorphMany
+    {
+        return $this->morphMany(TransactionConflicts::class, 'transaction');
+    }
+}
